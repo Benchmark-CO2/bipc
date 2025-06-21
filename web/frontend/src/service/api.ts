1199f5d860aac23bc5da6482bb35b653736cfb10
mateusscm <mateus.miranda@hotmail.com.br>
2025-06-21 14:50:37 -0300

    refactor: enhance request interceptor to handle user token parsing

--- web/frontend/src/service/api.ts
+++ web/frontend/src/service/api.ts
@@ -1,21 +1,47 @@
-import { BASE_URL } from '@/utils/constants';
-import axios from 'axios';
+import { BASE_URL } from "@/utils/constants";
+import { storageTokenKey } from "@/providers/authProvider";
+import axios from "axios";
 
 const api = axios.create({
   baseURL: BASE_URL as string,
-})
+});
 
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem(storageTokenKey)
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`
-//     }
-//     return config
-//   },
-//   (error: Error) => {
-//     return Promise.reject(error)
-//   }
-// )
+api.interceptors.request.use(
+  (config) => {
+    const storedUser = localStorage.getItem(storageTokenKey);
+    if (storedUser) {
+      try {
+        const parsedUser = JSON.parse(storedUser);
+        if (
+          parsedUser &&
+          typeof parsedUser === "object" &&
+          "token" in parsedUser
+        ) {
+          config.headers.Authorization = `Bearer ${parsedUser.token}`;
+        }
+      } catch (error) {
+        console.error("Error parsing stored user token:", error);
+      }
+    }
+    return config;
+  },
+  (error: Error) => {
+    return Promise.reject(error);
+  }
+);
 
-export default api
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem(storageTokenKey);
+
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
