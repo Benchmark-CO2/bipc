6e424158d0fd709048ec7d2dd82076fe021eec95
mateusscm <mateus.miranda@hotmail.com.br>
2025-06-21 14:51:25 -0300

    refactor: update project API endpoints to use versioned routes

--- web/frontend/src/actions/projects/getProjects.ts
+++ web/frontend/src/actions/projects/getProjects.ts
@@ -1,8 +1,6 @@
+import api from "@/service/api";
 import { IProject } from "@/types/projects";
 
 export const getAllProjectsByUser = () => {
-  // return api.get<{projects: IProject[]}>('/projects')
-  return new Promise<{data:{projects: IProject[]}} >((resolve) => {
-    resolve({data:{projects:[]} as {projects: IProject[]}})
-  })
-}
+  return api.get<{ projects: IProject[] }>("/v1/projects");
+};
--- web/frontend/src/actions/projects/postProject.ts
+++ web/frontend/src/actions/projects/postProject.ts
@@ -1,13 +1,9 @@
-import api from '@/service/api';
-import { AddProjectFormSchema } from '@/validators/addProject.validator';
+import api from "@/service/api";
+import { ProjectFormSchema } from "@/validators/project.validador";
 
-export const postProject = (projectParams: AddProjectFormSchema) => {
-  const formData = new FormData()
-  formData.append('name', projectParams.name)
-  formData.append('description', projectParams.description ?? '')
-  formData.append('city', projectParams.city)
-  formData.append('state', projectParams.state ?? '')
-  if (projectParams.image) formData.append('image', projectParams.image)
-  
-  return api.post<{ project_uuid: string; message: string }>('/projects', formData)
-}
+export const postProject = (projectParams: ProjectFormSchema) => {
+  return api.post<{ project_uuid: string; message: string }>(
+    "/v1/projects",
+    projectParams
+  );
+};
