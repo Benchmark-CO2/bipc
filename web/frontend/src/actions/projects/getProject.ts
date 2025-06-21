6610530d7f4b9e4f172eaa4596fe844bec0cb0d7
mateusscm <mateus.miranda@hotmail.com.br>
2025-06-21 16:40:07 -0300

    fix: correct project data structure in getProjectByUUID and update related route handling

--- web/frontend/src/actions/projects/getProject.ts
+++ web/frontend/src/actions/projects/getProject.ts
@@ -2,5 +2,5 @@ import api from "@/service/api";
 import { IProject } from "@/types/projects";
 
 export const getProjectByUUID = (projectId: string) => {
-  return api.get<{ projects: IProject }>(`/v1/projects/${projectId}`);
+  return api.get<{ project: IProject }>(`/v1/projects/${projectId}`);
 };
