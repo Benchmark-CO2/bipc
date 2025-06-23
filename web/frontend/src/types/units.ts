aa5e1c0cb417c537847a8b05185515b6fb321035
mateusscm <mateus.miranda@hotmail.com.br>
2025-06-22 22:45:44 -0300

    refactor: update TProjectUnit and IUnit types to use number for id and add type property

--- web/frontend/src/types/units.ts
+++ web/frontend/src/types/units.ts
@@ -1,8 +1,23 @@
-import { IModule } from './modules'
+import { IModule } from "./modules";
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export type Unit = {
-  id: string
-  title: string
-  modules: IModule[]
+  id: number;
+  title: string;
+  modules: IModule[];
+};
+
+export type TUnitType = "tower";
+
+export interface IUnit {
+  id: number;
+  project_id: number;
+  name: string;
+  type: TUnitType;
+  total_floors?: number;
+  tower_floors?: number;
+  base_floors?: number;
+  basement_floors?: number;
+  type_floors?: number;
+  total_area?: number;
 }
