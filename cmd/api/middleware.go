bf07f5a3602ea30dec4d867e09f98357d4ccbd49
Bruno Avelino <bruno.souza.avelino2@gmail.com>
2025-06-26 14:58:37 -0300

    deploy

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -117,7 +117,7 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 func (app *application) commonHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self'")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
