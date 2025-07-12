02a60a8ca17bf3b7b9f4bb1cf72939f765dc24d0
Anderson Alcará Alvarenga <anderalvarenga@gmail.com>
2025-07-12 19:48:48 -0300

    feat: route to create module and version

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -41,7 +41,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/projects/:projectID/units", app.requirePermission("project:edit", app.createUnitHandler))
 	router.HandlerFunc(http.MethodGet, "/v1/projects/:projectID/units/:unitID", app.requirePermission("project:view", app.readUnitHandler))
 
-	router.HandlerFunc(http.MethodPost, "/v1/projects/:projectID/units/:unitID/modules/:moduleID", app.requirePermission("project:edit", app.createVersionHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/projects/:projectID/units/:unitID/modules", app.requirePermission("project:edit" ,app.createModuleHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/projects/:projectID/units/:unitID/modules/:moduleID", app.requirePermission("project:edit" , app.createVersionHandler))
 
 	return app.metrics(app.recoverPanic(app.commonHeaders(app.enableCORS(app.rateLimit(app.authenticate(router))))))
 }
