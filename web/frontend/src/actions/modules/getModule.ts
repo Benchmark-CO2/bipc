3fe5219f67225a4956fd68dd548d3d3cba85139c
mateusscm <mateus.miranda@hotmail.com.br>
2025-07-16 18:47:10 -0300

    fix: update API response type in getModule function to use TModuleStructure

--- web/frontend/src/actions/modules/getModule.ts
+++ web/frontend/src/actions/modules/getModule.ts
@@ -1,11 +1,12 @@
 import api from "@/service/api";
+import { TModuleStructure } from "@/types/modules";
 
 export const getModule = (
   projectId: string,
   unitId: string,
   moduleId: string
 ) => {
-  return api.get<{ response: any }>(
+  return api.get<{ versions: TModuleStructure[] }>(
     `/v1/projects/${projectId}/units/${unitId}/modules/${moduleId}`
   );
 };
