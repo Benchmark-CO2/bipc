add4469ba5cd57575c24e3d3acf98cc3e1b0d456
mateusscm <mateus.miranda@hotmail.com.br>
2025-07-17 17:07:11 -0300

    feat: refactor route loader to use queryClient for module data retrieval

--- web/frontend/src/routes/_private/projects/$projectId/$unitId/$moduleId/route.tsx
+++ web/frontend/src/routes/_private/projects/$projectId/$unitId/$moduleId/route.tsx
@@ -1,37 +1,38 @@
-import { getFromStorage } from '@/lib/storage';
-import { TProjectUnitModule } from '@/types/projects';
-import { createFileRoute, Outlet } from '@tanstack/react-router';
-import { t } from 'i18next';
+import { getModule } from "@/actions/modules/getModule";
+import { structureTypes } from "@/utils/structureTypes";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
 
-const UNIT_MODULES = '@unit/modules';
 export const Route = createFileRoute(
-  '/_private/projects/$projectId/$unitId/$moduleId',
+  "/_private/projects/$projectId/$unitId/$moduleId"
 )({
   component: RouteComponent,
-  loader: ({ params }) => {
-    const { moduleId, unitId } = params
+  loader: async ({ params, context }) => {
+    const { moduleId, unitId } = params;
     if (!moduleId) {
-      throw new Error('Module ID is required')
+      throw new Error("Module ID is required");
     }
 
-    const unitModulesFromStorage = getFromStorage(
-      `${UNIT_MODULES}/${params.projectId}`,
-      {} as TProjectUnitModule,
-    )
-    const unit = unitModulesFromStorage[unitId]
+    const { data } = await context.queryClient.ensureQueryData({
+      queryKey: ["module", params.projectId, unitId, moduleId],
+      queryFn: () => getModule(params.projectId, unitId, moduleId),
+    });
 
-    const module = unit?.find((module) => module.module_uuid === moduleId)
+    if (!data || !data.versions) {
+      throw new Error("Module data not found");
+    }
+
+    const moduleType = data.versions[0]?.structure_type;
+
     return {
-      crumb: module?.tipoDeEstrutura ? t(`common.structureType.${module?.tipoDeEstrutura}`) : module?.tipoDeEstrutura,
-      module,
-    }
+      crumb: structureTypes[moduleType] || "Unknown",
+    };
   },
-})
+});
 
 function RouteComponent() {
   return (
     <>
       <Outlet />
     </>
-  )
+  );
 }
