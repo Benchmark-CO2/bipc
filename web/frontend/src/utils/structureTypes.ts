250278d66ff5207b16ea76900c8368ab7a60282d
mateusscm <mateus.miranda@hotmail.com.br>
2025-07-17 17:05:17 -0300

    feat: add structure types for concrete wall, beam column, and structural masonry

---
+++ web/frontend/src/utils/structureTypes.ts
@@ -0,0 +1,7 @@
+import { t } from "i18next";
+
+export const structureTypes = {
+  concrete_wall: t("common.structureType.concreteWall"),
+  beam_column: t("common.structureType.beamColumn"),
+  structural_masonry: t("common.structureType.masonry"),
+};
