2d9cf4df36d480daf1470fa91f3493a91be66437
mateusscm <mateus.miranda@hotmail.com.br>
2025-08-16 18:51:08 -0300

    fix: add margin to Divider component for improved spacing

--- web/frontend/src/components/ui/divider.tsx
+++ web/frontend/src/components/ui/divider.tsx
@@ -1,8 +1,5 @@
-
 const Divider = () => {
-  return (
-    <div className='w-full bg-zinc-500/70 h-[2px] rounded-md' />
-  )
-}
+  return <div className="w-full bg-zinc-500/70 h-[2px] rounded-md my-4" />;
+};
 
-export default Divider
+export default Divider;
