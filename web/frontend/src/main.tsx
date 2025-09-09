2ba5ab10a0f237ea5b75f3a99e350e545ed3a142
mateusscm <mateus.miranda@hotmail.com.br>
2025-09-09 18:19:04 -0300

    feat: add @radix-ui/react-tooltip dependency to enhance UI component functionality

--- web/frontend/src/main.tsx
+++ web/frontend/src/main.tsx
@@ -1,63 +1,65 @@
-import { ThemeProvider } from '@/components/theme-provider';
-import '@/index.css';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { createRouter, RouterProvider } from '@tanstack/react-router';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Toaster } from './components/ui/sonner';
-import { AuthContext } from './context/authContext';
-import { SummaryProvider } from './context/summaryContext';
-import { useAuth } from './hooks/useAuth';
-import './i18n';
-import { AuthProvider } from './providers/authProvider';
-import { routeTree } from './routeTree.gen';
-import { queryClient } from './utils/queryClient';
+import { ThemeProvider } from "@/components/theme-provider";
+import "@/index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "./components/ui/sonner";
+import { AuthContext } from "./context/authContext";
+import { SummaryProvider } from "./context/summaryContext";
+import { useAuth } from "./hooks/useAuth";
+import "./i18n";
+import { AuthProvider } from "./providers/authProvider";
+import { routeTree } from "./routeTree.gen";
+import { queryClient } from "./utils/queryClient";
+import { TooltipProvider } from "./components/ui/tooltip";
 const router = createRouter({
   routeTree,
-  defaultPreload: 'intent',
+  defaultPreload: "intent",
   context: {
     auth: {} as AuthContext,
-    queryClient
-  }
-})
+    queryClient,
+  },
+});
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
 export const App = () => {
-  const auth = useAuth()
+  const auth = useAuth();
   return (
     <RouterProvider
       router={router}
       context={{
-        auth
+        auth,
       }}
     />
-  )
-}
+  );
+};
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme='light'>
+    <ThemeProvider defaultTheme="light">
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <SummaryProvider>
-
-          <App />
-          <Toaster
-            position='bottom-right'
-            toastOptions={{
-              classNames: {
-                description: '!text-gray-600 !dark:text-gray-300'
-              }
-            }}
-          />
+            <TooltipProvider>
+              <App />
+              <Toaster
+                position="bottom-right"
+                toastOptions={{
+                  classNames: {
+                    description: "!text-gray-600 !dark:text-gray-300",
+                  },
+                }}
+              />
+            </TooltipProvider>
           </SummaryProvider>
         </AuthProvider>
       </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
-)
+);
