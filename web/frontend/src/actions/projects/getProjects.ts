91f0bc6344eb73f2a0deeeb1c8a23731c6152dc4
Felipe Okino <felipe1okino@live.com>
2025-09-15 18:59:58 -0300

    feat: increase page size

--- web/frontend/src/actions/projects/getProjects.ts
+++ web/frontend/src/actions/projects/getProjects.ts
@@ -2,5 +2,5 @@ import api from "@/service/api";
 import { IProject } from "@/types/projects";
 
 export const getAllProjectsByUser = () => {
-  return api.get<{ projects: IProject[] }>("/v1/projects");
+  return api.get<{ projects: IProject[] }>("/v1/projects/?page_size=50");
 };
