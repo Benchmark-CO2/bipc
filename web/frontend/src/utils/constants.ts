6de59bacdc422af3e549437352f5475f6fe0575b
Felipe Okino <felipe1okino@live.com>
2025-10-21 13:52:21 -0300

    chore: add default value for env variable

--- web/frontend/src/utils/constants.ts
+++ web/frontend/src/utils/constants.ts
@@ -1,4 +1,4 @@
-const { VITE_BASE_URL:BASE_URL, VITE_SITE_URL: PORTAL_URL, VITE_ENV:ENV='production', VITE_S3_BUCKET:BUCKET_URL } = import.meta.env
+const { VITE_BASE_URL:BASE_URL, VITE_SITE_URL: PORTAL_URL, VITE_ENV:ENV='production', VITE_S3_BUCKET:BUCKET_URL='https://bipc-avatar.s3.sa-east-1.amazonaws.com' } = import.meta.env
 
 export {
   BASE_URL, BUCKET_URL, ENV, PORTAL_URL
