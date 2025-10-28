5539a3d1c4b07110306ded642332d027dfa2ae24
mateusscm <mateus.miranda@hotmail.com.br>
2025-10-28 18:58:31 -0300

    refactor: simplify postSendInvite function and update validation schema

--- web/frontend/src/components/layout/drawer-invite.tsx
+++ web/frontend/src/components/layout/drawer-invite.tsx
@@ -1,8 +1,7 @@
 import { postSendInvite } from "@/actions/invites/postSendInvite";
-import { getAllProjectsByUser } from "@/actions/projects/getProjects";
 import { AddUserToProjectFormSchema } from "@/validators/addUserToProject.validator";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { CircleX, UserPlus } from "lucide-react";
 import { useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
@@ -26,21 +25,7 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
 
-const permissionsOptions: Array<{
-  value: string;
-  label: "drawerInvite.viewPermission" | "drawerInvite.editPermission";
-}> = [
-  { value: "project:view", label: "drawerInvite.viewPermission" },
-  { value: "project:edit", label: "drawerInvite.editPermission" },
-];
 const DrawerInvite = ({ projectId }: { projectId: string }) => {
   const ref = useRef<HTMLButtonElement>(null);
 
@@ -48,24 +33,13 @@ const DrawerInvite = ({ projectId }: { projectId: string }) => {
   const form = useForm({
     resolver: zodResolver(AddUserToProjectFormSchema),
     defaultValues: {
-      projectId: projectId,
       email: "",
-      permissions: [],
     },
   });
 
-  const { data: projects } = useQuery({
-    queryKey: ["projects"],
-    queryFn: getAllProjectsByUser,
-  });
-
   const { mutate } = useMutation({
-    mutationFn: ({
-      projectId,
-      email,
-      permissions,
-    }: AddUserToProjectFormSchema) =>
-      postSendInvite(projectId, email, permissions),
+    mutationFn: ({ email }: AddUserToProjectFormSchema) =>
+      postSendInvite(projectId, email),
     onSuccess: () => {
       toast.success(t("drawerInvite.title"), {
         description: t("drawerInvite.successMessage"),
@@ -84,9 +58,9 @@ const DrawerInvite = ({ projectId }: { projectId: string }) => {
   });
 
   const handleSubmit = (data: AddUserToProjectFormSchema) => {
-    const { projectId, email, permissions } = data;
+    const { email } = data;
 
-    mutate({ projectId, email, permissions });
+    mutate({ email });
   };
 
   useEffect(() => {
@@ -122,38 +96,6 @@ const DrawerInvite = ({ projectId }: { projectId: string }) => {
           >
             <FormField
               control={form.control}
-              name="projectId"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("drawerInvite.projectsLabel")}</FormLabel>
-                  <FormControl className="w-full">
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={String(field.value)}
-                    >
-                      <SelectTrigger className="w-full">
-                        <SelectValue
-                          placeholder={t("drawerInvite.projectsPlaceholder")}
-                        />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {projects?.data.projects.map((project) => (
-                          <SelectItem
-                            key={project.id}
-                            value={String(project.id)}
-                          >
-                            {project.name}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
               name="email"
               render={({ field }) => (
                 <FormItem>
@@ -165,47 +107,7 @@ const DrawerInvite = ({ projectId }: { projectId: string }) => {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name="permissions"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("drawerInvite.permissionsLabel")}</FormLabel>
-                  {permissionsOptions.map((permission, idx, self) => (
-                    <FormItem
-                      key={permission.value}
-                      className="flex items-center gap-2 pl-2"
-                    >
-                      <FormControl>
-                        <input
-                          type="checkbox"
-                          value={permission.value}
-                          checked={field.value.includes(permission.value)}
-                          onChange={(e) => {
-                            const value = e.target.value;
-                            if (e.target.checked) {
-                              field.onChange([
-                                ...new Set([
-                                  ...field.value,
-                                  ...self.slice(0, idx + 1).map((p) => p.value),
-                                ]),
-                              ]);
-                            } else {
-                              field.onChange(
-                                field.value.filter((v: string) => v !== value)
-                              );
-                            }
-                          }}
-                        />
-                      </FormControl>
 
-                      <FormLabel>{t(permission.label)}</FormLabel>
-                    </FormItem>
-                  ))}
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
             <Button type="submit" className="w-full mt-4">
               {t("drawerInvite.inviteButton")}
             </Button>
