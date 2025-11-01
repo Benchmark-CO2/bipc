b64f617dba4bf6ce2135dc1163ad7f22950bf351
mateusscm <mateus.miranda@hotmail.com.br>
2025-11-01 00:19:56 -0300

    feat: update sidebar links to use posLaunchFeatures and change menu items

--- web/frontend/src/components/layout/sidebar.tsx
+++ web/frontend/src/components/layout/sidebar.tsx
@@ -7,12 +7,11 @@ import { useSidebar } from "@/hooks/useSidebar";
 import { cn } from "@/lib/utils";
 import { Link } from "@tanstack/react-router";
 import {
-  Activity,
   BarChart3,
   Bell,
+  Book,
   Contact,
   File,
-  FlaskConical,
   Info,
   LogIn,
   Menu,
@@ -24,6 +23,7 @@ import { Notifications } from "../notifications";
 import { Button } from "../ui/button";
 import Divider from "../ui/divider";
 import SidemenuItem from "../ui/sidemenu-item";
+import { posLaunchFeatures } from "@/utils/posLaunchFeatures";
 
 interface ISidebar {
   handleLogout?: () => void;
@@ -64,27 +64,15 @@ const Sidebar = ({ handleLogout }: ISidebar) => {
             </SidemenuItem>
           </li>
 
-          {/* PD&I */}
           <li>
             <SidemenuItem variant="link">
               <Link
-                to={isAuthenticated ? "/benchmark" : "/login"}
+                to={posLaunchFeatures.trainingModal.formUrl}
+                target="_blank"
                 className="flex gap-3 items-center w-full p-2 hover:bg-zinc-700/30 rounded-md transition-colors"
               >
-                <FlaskConical size={18} />
-                <span>PD&I</span>
-              </Link>
-            </SidemenuItem>
-          </li>
-
-          <li>
-            <SidemenuItem variant="link">
-              <Link
-                to="/benchmark"
-                className="flex gap-3 items-center w-full p-2 hover:bg-zinc-700/30 rounded-md transition-colors"
-              >
-                <Activity size={18} />
-                <span>Ações</span>
+                <Book size={18} />
+                <span>Capacitação</span>
               </Link>
             </SidemenuItem>
           </li>
