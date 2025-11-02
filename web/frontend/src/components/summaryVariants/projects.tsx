23c1ddbe5afd70e9d82d074b1b1b8a369e8ab182
Felipe Okino <felipe1okino@live.com>
2025-11-01 22:58:01 -0300

    style: adjust legend

--- web/frontend/src/components/summaryVariants/projects.tsx
+++ web/frontend/src/components/summaryVariants/projects.tsx
@@ -123,7 +123,7 @@ const ProjectsSummary = ({ projects, data, someSelected }: ProjectsSummaryProps)
         />
       </div>
       <div
-        className={cn("w-full flex justify-between gap-4 max-md:flex-col h-[85%] max-sm:h-max", {
+        className={cn("w-full flex justify-between gap-4 max-md:flex-col 2xl:h-[85%] max-sm:h-max", {
           "flex flex-col": isExpanded,
         })}
       >
