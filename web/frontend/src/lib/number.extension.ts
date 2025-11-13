ef033c42e45a1645b7ef71e1015bd6cc3d12f4ac
Felipe Okino <felipe1okino@live.com>
2025-11-12 21:29:27 -0300

    hotfix: adjust number extension func

--- web/frontend/src/lib/number.extension.ts
+++ web/frontend/src/lib/number.extension.ts
@@ -1,5 +1,6 @@
 Object.defineProperty(Number.prototype, 'toInternational', {
   value: function(locale: string = 'pt-BR', decimals: number = 1) {
+    if (!this) return this
     if (isNaN(this)) {
       return 0;
     }
