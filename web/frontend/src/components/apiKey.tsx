5f220e2ff45e22cbe68823a0e23e50d8e5405cd0
Felipe Okino <felipe1okino@live.com>
2026-01-15 02:21:15 -0300

    chore: change parent tag

--- web/frontend/src/components/apiKey.tsx
+++ web/frontend/src/components/apiKey.tsx
@@ -26,10 +26,10 @@ export const GenerateApiKey = () => {
   return (
     <div>
       <Button variant="link" className="p-0 h-auto" onClick={generateApiKey}>Gerar nova chave de API <RefreshCcw  /></Button>
-      <p className="text-sm text-muted-foreground flex items-center gap-2 mt-2">
+      <div className="text-sm text-muted-foreground flex items-center gap-2 mt-2">
         
         {apiKey && <div className='flex flex-col gap-1'><span className='font-bold mt-2'>A chave de API é de visualização única, copie-a agora pois não será possível vê-la novamente.:</span> <span className="font-bold flex items-center gap-2">{visibleApiKey ? apiKey : "*****************"}<Copy className='cursor-pointer' size={16} onClick={handleCopy} /></span></div>}
-      </p>
+      </div>
     </div>
   );
 };
