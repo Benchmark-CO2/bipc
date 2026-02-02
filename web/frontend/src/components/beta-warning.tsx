540a6bbfe8346738880d34aeeff087b4734f8171
mateusscm <mateus.miranda@hotmail.com.br>
2026-02-02 16:08:00 -0300

    fix: enhance styling and improve contact information presentation in beta warning component

--- web/frontend/src/components/beta-warning.tsx
+++ web/frontend/src/components/beta-warning.tsx
@@ -5,12 +5,19 @@ export const BetaWarning = () => {
     <div className="bg-primary text-white p-2 px-4 rounded-lg mx-auto">
       <div className="flex items-center gap-2 my-1">
         <FlaskConical size={16} />
-        <span>Estamos em beta</span>
+        <span className="font-bold">Estamos em beta</span>
       </div>
-      <p className="text-sm ">
+      <p className="text-sm mb-1">
         A plataforma segue em desenvolvimento e recebe melhorias constantes.
-        Para sugestões, entre em contato pelo email:{" "}
-        <strong>contato@bipc.org.br</strong>
+      </p>
+      <p className="text-sm ">
+        Tem alguma sugestão de melhoria? Envie um e-mail para:{" "}
+        <a
+          href="mailto:contato@bipc.org.br"
+          className="font-bold hover:underline"
+        >
+          contato@bipc.org.br
+        </a>
       </p>
     </div>
   );
