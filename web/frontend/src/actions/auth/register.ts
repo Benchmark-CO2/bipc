68140ae10d53294a2ff62c5ebe3f3a21d3dd60a1
mateusscm <mateus.miranda@hotmail.com.br>
2026-04-01 18:22:26 -0300

    feat: add company/member user sign-up

--- web/frontend/src/actions/auth/register.ts
+++ web/frontend/src/actions/auth/register.ts
@@ -4,11 +4,18 @@ interface TAuthParams {
   name: string;
   email: string;
   password: string;
+  type: "member" | "company";
+  cnpj?: string;
   crea_cau?: string;
   birthdate?: string;
   city?: string;
   activity?: string;
   enterprise?: string;
+  cep?: string;
+  state?: string;
+  neighborhood?: string;
+  street?: string;
+  number?: string;
 }
 
 export const register = (authParams: TAuthParams) => {
