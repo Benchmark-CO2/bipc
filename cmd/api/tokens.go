5fe26f6d05e2ce648e711155ce3aee41e7e50403
Bruno Avelino <bruno.souza.avelino2@gmail.com>
2026-07-14 23:16:39 -0300

    generate token jwt

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -9,7 +9,23 @@ import (
 
 	"github.com/Benchmark-CO2/bipc/internal/data"
 	"github.com/Benchmark-CO2/bipc/internal/validator"
+	"github.com/google/uuid"
+
+	"github.com/golang-jwt/jwt/v5"
 )
+
+func GenerateTokenJWT(userID uuid.UUID, key string) (string, error) {
+	claims := jwt.RegisteredClaims{
+		Subject:   userID.String(),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		Issuer:    "app.bipc.org.br",
+		Audience:  jwt.ClaimStrings{"ifc-service"},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(key))
+}
 
 func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
@@ -69,7 +85,13 @@ func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, 
 		return
 	}
 
-	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token, "user": user}, nil)
+	tokenJWT, err := GenerateTokenJWT(user.ID, app.config.jwt.secret)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token, "user": user, "jwt": tokenJWT}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
